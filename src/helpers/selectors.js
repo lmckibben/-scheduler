3df3dc3c7bc41aedbba4bf53ea877c860a19ebeb
Leland <lmckibben28@hotmail.com>
2021-09-30 16:26:48 -0700

    created helper function and test to select data from days and appointments

---
+++ src/helpers/selectors.js
@@ -0,0 +1,21 @@
+export function getAppointmentsForDay(state, day) {
+  const dayArray = [];
+  const appointmentArray = [];
+  for (let dayState of state.days) {
+    if (dayState.appointments.length === 0) {
+      return dayArray;
+    }
+    if (dayState.name === day) {
+      dayArray.push(...dayState.appointments);
+    }
+  }
+
+  for (let appointment in state.appointments) {
+    for (let day of dayArray) {
+      if (day === parseInt(appointment)) {
+        appointmentArray.push(state.appointments[appointment]);
+      }
+    }
+  }
+  return appointmentArray;
+}
