b79270d3a77ee21f6e8de94b7317c29eec4775e1
Leland <lmckibben28@hotmail.com>
2021-10-06 16:04:45 -0700

    added data-itemid

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -22,7 +22,7 @@ export default function DayListItem(props) {
     "day-list__item--full": props.spots === 0
   });
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} data-testid="day" onClick={() => props.setDay(props.name) }>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{spots}</h3>
     </li>
